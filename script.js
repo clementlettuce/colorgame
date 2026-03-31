09c07841afaf3dd09efd956dc311f4e82177577b
clementlettuce <clement103283@gmail.com>
2026-03-30 21:25:15 -0700

    create items

--- script.js
+++ script.js
@@ -10,4 +10,21 @@ workspace.addEventListener('drop', (e) => {
   const r = parseInt(currentDrag.dataset.r);
   const g = parseInt(currentDrag.dataset.g);
   const b = parseInt(currentDrag.dataset.b);
+  const name = currentDrag.textContent;
+  createCircle(e.offsetX, e.offsetY, r, g, b, name);
+
 });
+function createCircle(x, y, r, g, b, name) {
+  const circle = document.createElement('div');
+  circle.classList.add('circle');
+  circle.style.left = x + 'px';
+  circle.style.top = y + 'px';
+  circle.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
+  circle.dataset.r = r;
+  circle.dataset.g = g;
+  circle.dataset.b = b;
+  circle.textContent = name;
+  makeDraggable(circle);
+  workspace.appendChild(circle);
+}
+
